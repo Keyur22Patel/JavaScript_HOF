596ec2233531941be3d77204050007ac0ef93112
_Keyur Patel_ <_[keyur22p@gmail.com]_>
2022-06-14 13:57:28 -0500

    HOF

--- app.js
+++ app.js
@@ -1,4 +1,72 @@
-console.log("Hello World!\n==========\n");
+
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+function plus(number){
+    return function (plusNumber){
+return number + plusNumber;
+    };
+    
+}
+const plus2 = plus(2); 
+
+console.log(plus2(2));
+console.log(plus2(5));
+
+const plus100 = plus(100);
+
+console.log(plus100(100));
+console.log(plus100(5));
+// Exercise 1 Secction
+
+let users = [
+    {
+      name: "Frodo",
+      age: 50,
+      score: 85,
+      isActive: false,
+    },
+    {
+      name: "Sam",
+      age: 38,
+      score: 94,
+      isActive: true,
+    },
+    {
+      name: "Merry",
+      age: 36,
+      score: 82,
+      isActive: true,
+    },
+    {
+      name: "Pippin",
+      age: 26,
+      score: 77,
+      isActive: false,
+    },
+  ];
+   users.forEach(users => console.log(users.name));
+
+     //Map
+
+    let newUsers = users.map((user) =>{
+        return { name: users.name, score: user.score}
+    })
+   console.log(newUsers)
+
+  // filetr
+
+  let activeUsers = users.filter((user) => 
+    user.isActive);
+  console.log(activeUsers);
+
+  //sort 
+
+  users.sort((a, b) => {
+    if (a.score < b.score) return 1;
+    else return -1;
+  })
+  console.log(users);
+
+  //reduce
+  let avgScore = users.reduce((sum, user) => sum +user.score, 0);
+  console.log(avgScore)
